f9407100209794b4d6c35bc191cd9407c89327e6
Yeeway69 <yeyiwei1@gmail.com>
2024-10-26 14:14:24 +0200

    audiomanager plus some audio assets

---
+++ MyArkanoid/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,72 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+
+    [SerializeField] private AudioSource backgroundMusicSource;
+    [SerializeField] private AudioSource sfxSource;
+   
+
+    [Header("---------- Audio Clips ----------")]
+    public AudioClip backgroundMusic;
+    public AudioClip select;
+    public AudioClip hit;
+    public AudioClip die;
+    public AudioClip complete;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        PlayBackgroundMusic(backgroundMusic, transform, 0.5f);
+
+    }
+
+    private void PlayBackgroundMusic(AudioClip audioClip, Transform spawnTransform, float volume)
+    {
+        AudioSource audioSource = Instantiate(backgroundMusicSource, spawnTransform.position, Quaternion.identity);
+
+        audioSource.clip = audioClip;
+
+        audioSource.volume = volume;
+
+        audioSource.Play();
+
+    }
+
+    public void PlaySoundFXClip(AudioClip audioClip, Transform spawnTransform, float volume)
+    {
+        AudioSource audioSource = Instantiate(sfxSource, spawnTransform.position, Quaternion.identity);
+
+        audioSource.clip = audioClip;
+
+        audioSource.volume = volume;
+
+        audioSource.Play();
+
+        float clipLength = audioClip.length;
+
+        Destroy(audioSource.gameObject, clipLength);
+    }
+
+    
+    
+
+
+}
